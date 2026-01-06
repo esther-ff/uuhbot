c311b73c3a05b1ad2f0ae5ff734761b07ac2741c
esther-ff <esther0x@proton.me>
2026-01-07 00:15:58 +0100

    feat: config.ts

---
+++ config.ts
@@ -0,0 +1,13 @@
+export const botConfig = {
+  server: "mc.hypixel.net",
+  botMcVersion: "1.8.1",
+  botUser: "BallinBridge",
+  discordLogs: null,
+  minecraftLogs: null,
+  guildId: "1167183789961773106",
+  discordToken: process.env.BIGEON_TOKEN,
+  hypixelApiKey: "b1ed24b4-6e88-4ac7-8767-bf349b33b80d",
+  embedColor: "ff69b4",
+ 
+  channelId: "1286687362281242736",
+}
