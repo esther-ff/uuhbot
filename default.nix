b53702ab2eba673aa8b3b4d9dc60cdd7c2347546
esther-ff <esther0x@proton.me>
2026-01-07 01:12:07 +0100

    feat: working flake?

--- default.nix
+++ default.nix
@@ -1,24 +1,36 @@
-{ stdenv, fetchFromGitHub, bun }:
+{ buildNpmPackage, fetchNpmDeps, fetchFromGitHub, bun }:
 
-stdenv.mkDerivation {
+buildNpmPackage {
   name = "bigeon";
   version = "0.0.1";
+  npmDepsHash = "sha256-PmJ5d7Ic9qqCNvaLALUzjvUlskfUq0vX8H4iCpCxkXY=";
+  makeCacheWritable = true;
+  dontNpmBuild = true;
 
-  src = fetchFromGitHub {
-    owner = "esther-ff";
-    repo = "uuhbot";
-    rev = "main";
-    hash = "sha256-u9nSCa5gzsuYmRhJJrpkb+nK1NHZFjC3uTy32cg8jSE=";
-  };
+  # src = fetchFromGitHub {
+  #   owner = "esther-ff";
+  #   repo = "uuhbot";
+  #   rev = "5db77c2daa7b9c9e24f42093ad217bcd2b7f6014";
+  #   hash = "sha256-MnrXI1OfSHhMz8DlvdL/9doX170HBPK1V28DB1OZb5I=";
+  # };
+  #
+
+  src = ./.;
 
   buildInputs = [ bun ];
+  buildPhase = ''
+    runHook preBuild
+    mkdir -p $out
+    ${bun}/bin/bun build ./main.ts --target=bun --outfile=$out/bigeon.ts
+
+    runHook postBuild
+  '';
 
   installPhase = ''
     runHook preInstall
-    cd $src
-    ls config.ts
-    bun build ./main.ts --target=bun --minify --outfile=$out/.bigeon.ts 
-    echo "${bun} run $out/.bigeon.ts --smol" > $out/bin/bigeon
+    mkdir -p $out/bin
+
+    echo "${bun}/bin/bun run $out/bigeon.ts --smol" > $out/bin/bigeon
     chmod 744 $out/bin/bigeon
     runHook postInstall
   '';
